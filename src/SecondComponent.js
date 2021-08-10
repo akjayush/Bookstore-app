3057c3544bd6ef5e10911d9742ae827eec5d0b49
akjayush <ayushjha110@gmail.com>
2021-08-10 10:54:48 +0530

    completed my second component and add a image

---
+++ src/SecondComponent.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function SecondComponent() {
+    return (
+        <div>
+            <h1>I am a rebel</h1>
+        </div>
+    )
+}
+
+export default SecondComponent
--- src/firstComponent.js
+++ src/firstComponent.js
@@ -1,15 +1,15 @@
-import react from "react";
+import React from "react";
 import {render} from "react-dom";
 
 function FirstComponent() {
     return(
-        <react.Fragment>
+        <React.Fragment>
         <div>
-            <div>
-                <h2>This is my First Component</h2>
-            </div>   
+            <h2>This is my First Component</h2>
+            <img src="penguin.jpg" alt="the penguin should have been here but it isn't"
+            width="500" height="320"></img>
         </div>
-        </react.Fragment>
+        </React.Fragment>
     )
 }
 
