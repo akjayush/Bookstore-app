94e172fdf41b494fbb919e89bdf4e5f21e38cbef
akjayush <ayushjha110@gmail.com>
2021-08-10 09:58:03 +0530

    created my first component on react

---
+++ src/firstComponent.js
@@ -0,0 +1,16 @@
+import react from "react";
+import {render} from "react-dom";
+
+function FirstComponent() {
+    return(
+        <react.Fragment>
+        <div>
+            <div>
+                <h2>This is my First Component</h2>
+            </div>   
+        </div>
+        </react.Fragment>
+    )
+}
+
+export default FirstComponent
